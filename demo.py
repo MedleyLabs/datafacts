23e49f1f15b5acc88a2631a5ba36b690baacb618
eric-yates <eric@medleyagency.com>
2021-10-28 12:23:41 -0600

    Update demo

--- demo.py
+++ demo.py
@@ -1,9 +1,10 @@
+import os
 import pandas as pd
 
 from datafacts import generate_img
 
 
-if __name__ == '__main__':
+def main():
 
     df = pd.DataFrame.from_dict({
         'Ethnic composition': ['Non-Hispanic White'] * 60 +
@@ -11,7 +12,7 @@ if __name__ == '__main__':
                               ['Black or African American'] * 13 +
                               ['Asian'] * 6 +
                               ['Other'] * 3,
-        'Gender composition': ['Male'] * 55 + ['Female'] * 45,
+        'Gender composition': ['Male'] * 52 + ['Female'] * 45 + ['Non-Binary'] * 3,
         'Manufacturer and model': ['GE Optima 540'] * 72 + ['Siemens Scope 16'] * 28,
     })
 
@@ -25,4 +26,12 @@ if __name__ == '__main__':
         'Algorithm AUC': {'Training data': '0.94', 'Validation data': '0.91'}
     }
 
-    generate_img(df, 'img.png', cols=cols, sections=sections, summary_description=text)
+    if not os.path.exists('img/'):
+        os.mkdir('img')
+
+    generate_img(df, 'img/datafacts.png', cols=cols, sections=sections, summary_description=text)
+
+
+if __name__ == '__main__':
+
+    main()
